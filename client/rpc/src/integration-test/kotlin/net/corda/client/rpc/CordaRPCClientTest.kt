90ce14835c5d8077704b6b91f41b40cf4bc16245
Chris Rankin <chris.rankin@r3.com>
2017-04-06 16:47:40 +0100

    Remove progress Observable from FlowHandle, unless explicitly requested.

--- client/rpc/src/integration-test/kotlin/net/corda/client/rpc/CordaRPCClientTest.kt
+++ client/rpc/src/integration-test/kotlin/net/corda/client/rpc/CordaRPCClientTest.kt
@@ -4,6 +4,7 @@ import net.corda.core.contracts.DOLLARS
 import net.corda.core.flows.FlowException
 import net.corda.core.getOrThrow
 import net.corda.core.messaging.startFlow
+import net.corda.core.messaging.startFlowWithProgress
 import net.corda.core.node.services.ServiceInfo
 import net.corda.core.random63BitValue
 import net.corda.core.serialization.OpaqueBytes
@@ -68,7 +69,7 @@ class CordaRPCClientTest : NodeBasedTest() {
         println("Creating proxy")
         val proxy = client.proxy()
         println("Starting flow")
-        val flowHandle = proxy.startFlow(
+        val flowHandle = proxy.startFlowWithProgress(
                 ::CashIssueFlow,
                 20.DOLLARS, OpaqueBytes.of(0), node.info.legalIdentity, node.info.legalIdentity)
         println("Started flow, waiting on result")
@@ -104,9 +105,7 @@ class CordaRPCClientTest : NodeBasedTest() {
                 node.info.legalIdentity, node.info.legalIdentity
         )
         println("Started issuing cash, waiting on result")
-        flowHandle.progress.subscribe {
-            println("CashIssue PROGRESS $it")
-        }
+        flowHandle.returnValue.get()
 
         val finishCash = proxy.getCashBalances()
         println("Cash Balances: $finishCash")
